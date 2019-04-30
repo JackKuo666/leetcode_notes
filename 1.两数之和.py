0eaaf2c764ed5b21791db928854a89e75c872f67
JackKuo666 <41313632+JackKuo666@users.noreply.github.com>
2019-04-30 20:11:35 +0800

    Update 1.两数之和.py

--- 1.两数之和.py
+++ 1.两数之和.py
@@ -6,14 +6,14 @@ class Solution:
         :rtype: List[int]
         """
 
-        # 法1.暴力遍历法
+        # 法1.暴力遍历法O（n^2）
         # for i in range(len(nums)):
         #     for j in range(i+1, len(nums)):
         #         if nums[i] + nums[j] == target:
         #             return [i, j]
         # return [-1, -1]
         
-        # 法2.hashmap
+        # 法2.hashmap  时间复杂度O（n）
         dic = {}
         for i in range(len(nums)):
             if target - nums[i] in dic:
@@ -22,7 +22,7 @@ class Solution:
                 dic[nums[i]] = i
         return [-1, -1]
         
-        # 法3.sort and 两个指针
+        # 法3.先枚举一下，然后sort,然后使用两个指针，时间复杂度远大于O（n），所以如果是无序的输入，使用双指针不合适
 #         new_nums = [(v, index) for index, v in enumerate(nums)]
 #         new_nums.sort()
 #         left, right = 0, len(nums) - 1
