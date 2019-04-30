5f3f78d9682b586be63a2c3fff3962ba8e792737
JackKuo666 <41313632+JackKuo666@users.noreply.github.com>
2019-04-30 18:53:42 +0800

    Create 1.两数之和.py

---
+++ 1.两数之和.py
@@ -0,0 +1,26 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # # 法1.暴力求解
+        # for i in range(len(nums) - 1):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+        
+        # 法2.使用字典        
+        if len(nums) <= 1:
+            return False 
+        buff_dict = {}                 # key 是target-nums[i],  value 是 i 的位置
+        for i in range(len(nums)):
+            if nums[i] in buff_dict:
+                return [buff_dict[nums[i]], i]
+            else:
+                buff_dict[target - nums[i]] = i
+                
+                
+                
+                
