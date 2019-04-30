0e50c7dcfdce0b8101354c8cecf07295503a7732
JackKuo666 <41313632+JackKuo666@users.noreply.github.com>
2019-04-30 19:50:38 +0800

    Update 1.两数之和.py

--- 1.两数之和.py
+++ 1.两数之和.py
@@ -5,22 +5,35 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
-        # # 法1.暴力求解
-        # for i in range(len(nums) - 1):
-        #     for j in range(i + 1, len(nums)):
+
+        # 法1.暴力遍历法
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
         #         if nums[i] + nums[j] == target:
         #             return [i, j]
+        # return [-1, -1]
         
-        # 法2.使用字典        
-        if len(nums) <= 1:
-            return False 
-        buff_dict = {}                 # key 是target-nums[i],  value 是 i 的位置
+        # 法2.hashmap
+        dic = {}
         for i in range(len(nums)):
-            if nums[i] in buff_dict:
-                return [buff_dict[nums[i]], i]
+            if target - nums[i] in dic:
+                return [dic[target - nums[i]], i]
             else:
-                buff_dict[target - nums[i]] = i
-                
+                dic[nums[i]] = i
+        return [-1, -1]
+        
+        # 法3.sort and 两个指针
+#         new_nums = [(v, index) for index, v in enumerate(nums)]
+#         new_nums.sort()
+#         left, right = 0, len(nums) - 1
+#         while left < right:
+#             if new_nums[left][0] + new_nums[right][0] < target:
+#                 left += 1
+#             elif new_nums[left][0] + new_nums[right][0] > target:
+#                 right -= 1
+#             else:
+#                 return [new_nums[left][1], new_nums[right][1]]
+#         return [-1, -1]
                 
                 
                 
