e81a3408ac6dd5d839b0d822c214862b2901df83
JackKuo666 <41313632+JackKuo666@users.noreply.github.com>
2019-05-24 20:17:31 +0800

    Create 027.删除元素.py

---
+++ Python/027.删除元素.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        i = 0
+        while i < len(nums):
+            if nums[i] == val:
+                del nums[i]
+                i -= 1
+            i += 1
+        return len(nums)
