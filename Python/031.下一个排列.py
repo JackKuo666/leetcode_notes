a2d5932f917a7309e62ad1e000a4b418dfc52798
JackKuo666 <41313632+JackKuo666@users.noreply.github.com>
2019-05-27 10:18:58 +0800

    Create 031.下一个排列.py

---
+++ Python/031.下一个排列.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        # 法1. O(nk) and O(1)
+        if len(nums) <= 1:
+            return
+        for i in range(len(nums) - 2, -1, -1):
+            if nums[i] < nums[i + 1]:
+                for k in range(len(nums) - 1, i, -1):
+                    if nums[k] > nums[i]:
+                        nums[i], nums[k] = nums[k], nums[i]
+                        nums[i+1:] = sorted(nums[i+1:])
+                        break
+                break
+            
+            else:
+                if i == 0:
+                    nums.sort()
+                
+            
